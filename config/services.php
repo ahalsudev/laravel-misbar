570d8b9b5324c1b90aa80098ee5519a6ecf26a47
Ahmad <ahalsu67@gmail.com>
2025-09-13 17:48:59 +0300

    Initial commit

--- config/services.php
+++ config/services.php
@@ -1,44 +1,38 @@
 <?php
 
 return [
-    'misbar' => [
-        'backend_url' => env('MISBAR_BACKEND_URL'),
-        'api_key' => env('MISBAR_API_KEY'),
-        'source' => env('MISBAR_SOURCE', 'laravel-trading-app'),
-        'enabled' => env('MISBAR_ENABLED', true),
-        'sampling_rate' => env('MISBAR_SAMPLING_RATE', 1.0),
-        'redact_sensitive_headers' => true,
-        'max_body_size' => 10000,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
     ],
 
-    'alpaca' => [
-        'api_key' => env('ALPACA_API_KEY'),
-        'secret_key' => env('ALPACA_SECRET_KEY'),
-        'base_url' => env('ALPACA_BASE_URL', 'https://paper-api.alpaca.markets'),
-        'data_url' => env('ALPACA_DATA_URL', 'https://data.alpaca.markets'),
-        'version' => 'v2',
-        'paper_trading' => env('ALPACA_PAPER_TRADING', true),
+    'resend' => [
+        'key' => env('RESEND_KEY'),
     ],
 
-    'alpha_vantage' => [
-        'api_key' => env('ALPHA_VANTAGE_API_KEY'),
-        'base_url' => 'https://www.alphavantage.co/query',
-        'rate_limit' => 5, // requests per minute
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
-    'news_api' => [
-        'api_key' => env('NEWS_API_KEY'),
-        'base_url' => 'https://newsapi.org/v2',
-        'sources' => [
-            'bloomberg',
-            'financial-post',
-            'fortune',
-            'the-wall-street-journal',
+    'slack' => [
+        'notifications' => [
+            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
+            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
 
-    'coingecko' => [
-        'base_url' => 'https://api.coingecko.com/api/v3',
-        'rate_limit' => 50, // requests per minute for free tier
-    ],
 ];
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test-api', function() {
+    $payload = [
+        'test' => 'This is a much longer JSON payload that should definitely require HTTP/2 DATA frames to be sent properly and trigger the debug output'
+    ];
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://api.misbar.tech/api/test-200');
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json'
+    ]);
+    curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2_0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    return response()->json([
+        'status' => $httpCode,
+        'response' => $response
+    ]);
+});
